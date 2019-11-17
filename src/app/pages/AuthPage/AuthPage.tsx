b56b3b9066ceaf87f28369c691b2cdb52f05c73b
Tarique Ejaz <tariqueejaz.93102@gmail.com>
2019-11-17 15:09:24 +0700

    basic auth changes

--- src/app/pages/AuthPage/AuthPage.tsx
+++ src/app/pages/AuthPage/AuthPage.tsx
@@ -19,28 +19,49 @@ class AuthPage extends PureComponent<AuthProps, AuthState> {
     }
 
     render() {
+        const Login = (<div className="Login">
+            <div className="AuthContainer_Input">
+                <input className="Input" type="text" onBlur={() => { }} placeholder="Username/Email" />
+                <input className="Input" type="password" onBlur={() => { }} placeholder="Password" />
+            </div>
+
+            <div className="AuthContainer_Button">
+                <button className="Button">Sign In</button>
+                <button className="Button Button--Inactive" onClick={() => this.setState({ login: false })}>Sign Up</button>
+            </div>
+
+            <div className="AuthContainer_Forgot">
+                <p className="Link">Forgot Password</p>
+            </div>
+        </div>);
+
+        const Signup = (
+            <div className="Signup">
+                <div className="AuthContainer_Input">
+                    <input className="Input" type="text" onBlur={() => { }} placeholder="Enter Email" />
+                    <input className="Input" type="text" onBlur={() => { }} placeholder="Enter Name" />
+                    <input className="Input" type="password" onBlur={() => { }} placeholder="Enter Password" />
+                    <input className="Input" type="password" onBlur={() => { }} placeholder="Confirm Password" />
+                </div>
+
+                <div className="AuthContainer_Button">
+                    <button className="Button">Sign Up</button>
+                </div>
+
+                <div className="AuthContainer_Forgot">
+                    <p className="Link" onClick={() => this.setState({ login: true })}>Go Back To Sign In</p>
+                </div>
+            </div>
+        );
+
         return (
             <div className="AuthPage">
-                <div className="Login">
-                    <div className="AuthContainer">
-                        <div className="AuthContainer_Logo">
-                            <h2 className="Logo">Nicole</h2>
-                        </div>
-
-                        <div className="AuthContainer_Input">
-                            <input className="Input" type="text" onBlur={() => { }} placeholder="Username/Email" />
-                            <input className="Input" type="password" onBlur={() => { }} placeholder="Password" />
-                        </div>
-
-                        <div className="AuthContainer_Button">
-                            <button className="Button">Sign In</button>
-                            <button className="Button Button--Inactive">Sign Up</button>
-                        </div>
-
-                        <div className="AuthContainer_Forgot">
-                            <p className="Link">Forgot Password</p>
-                        </div>
+                <div className="AuthContainer">
+                    <div className="AuthContainer_Logo">
+                        <h2 className="Logo">Nicole</h2>
                     </div>
+
+                    {this.state.login ? Login : Signup}
                 </div>
             </div>
         );
