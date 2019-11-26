fdadcbca316580171ac4f1cb13d86147361c15e4
Tarique Ejaz <tariqueejaz.93102@gmail.com>
2019-11-26 21:31:10 +0700

    single product layout

--- src/app/pages/SingleProductPage/SingleProductPage.tsx
+++ src/app/pages/SingleProductPage/SingleProductPage.tsx
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import './SingleProductPage.scss';
 import Header from '../../components/Header/Header';
 import DefaultProduct from '../../../assets/images/default-product.svg';
-import { LineChart } from 'recharts';
+import { LineChart, ResponsiveContainer, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line, ReferenceLine, AreaChart, Area } from 'recharts';
 import Button from '../../components/Button/Button';
 
 interface SingleProductProps {
@@ -23,6 +23,19 @@ class SingleProductPage extends PureComponent<SingleProductProps, SingleProductS
     }
 
     render() {
+
+        const mockData = [
+            { label: '2019-11-25', price: 12 },
+            { label: '2019-11-26', price: 20000 },
+            { label: '2019-11-27', price: 1233 },
+            { label: '2019-11-29', price: 9000 },
+            { label: '2019-11-30', price: 23000 },
+            { label: '2019-12-10', price: 11220 },
+            { label: '2019-12-11', price: 10 },
+            { label: '2019-12-13', price: 92 },
+            { label: '2019-11-15', price: 356 },
+        ]
+
         return (
             <div className="SingleProductPage">
                 <Header
@@ -89,7 +102,26 @@ class SingleProductPage extends PureComponent<SingleProductProps, SingleProductS
                     </div>
 
                     <div className="Statistics">
-                        <p className="Statistics_Title">Price Trend</p>
+                        <p className="Statistics_Title">Price Trend (RS)</p>
+
+                        <div className="Statistics_Graph">
+                            {(mockData.length > 0) ? (
+                                <ResponsiveContainer width="100%">
+                                    <AreaChart height={500} data={mockData}
+                                        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                                        <CartesianGrid strokeDasharray="3 3" />
+                                        <XAxis dataKey="label" />
+                                        <YAxis />
+                                        <Tooltip />
+                                        <ReferenceLine y={10000} stroke="red" strokeDasharray="3 3" />
+                                        <Legend />
+                                        <Area type="monotone" dataKey="price" stroke="black" strokeWidth={3} fillOpacity={0.3} fill="black" />
+                                    </AreaChart>
+                                </ResponsiveContainer>
+                            ) : (
+                                    <p className="Statistics_DefaultMessage">No data records currently present</p>
+                                )}
+                        </div>
                     </div>
                 </div>
             </div>
